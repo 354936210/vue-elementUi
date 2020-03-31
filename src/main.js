2889ae4d852967af787596247e9b0a86e67ee142
hellobird <hellobird@163.com>
2020-03-31 12:50:04 +0800

    添加markdown功能

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import App from './App.vue'
 import router from './router'
 // eslint-disable-next-line no-unused-vars
 import axios from 'axios'
-
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+// use
+Vue.use(mavonEditor)
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <el-button type="primary" size="mini" @click="getAlluser">主要按钮</el-button>
+    <mavon-editor @change = "changeMarkstr" @save="saveMarkstr" v-model="savestr"/>
   </div>
 </template>
 
@@ -13,10 +14,18 @@ export default {
   },
   data () {
     return {
+      savestr: ''
     }
   },
   methods: {
+    saveMarkstr: function (value, render) {
+      console.log(render)
+    },
+    changeMarkstr: function (value, render) {
+
+    },
     getAlluser: function () {
+      console.log(this.savestr)
       this.$axios.get('http://localhost:8080//user/getAll').then(
         function (result) {
           console.log(JSON.stringify(result.data))
